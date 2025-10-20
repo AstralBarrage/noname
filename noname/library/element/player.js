a6f99b423279c7804d4e7eefceb7bf2f4e791994
AstralBarrage <hoshimimaidono@gmail.com>
2025-10-21 02:48:21 +0800

    华裳调整；华裳、工巧、纵马素材补充；restoreSkill调整

--- noname/library/element/player.js
+++ noname/library/element/player.js
@@ -10344,7 +10344,7 @@ export class Player extends HTMLDivElement {
 		}
 		this.awakenedSkills.remove(skill);
 		this.enableSkill(skill + "_awake", skill);
-		if (!nomark) {
+		if (!nomark && this.hasSkill(skill, null, null, false)) {
 			this.markSkill(skill);
 		}
 		_status.event.clearStepCache();
